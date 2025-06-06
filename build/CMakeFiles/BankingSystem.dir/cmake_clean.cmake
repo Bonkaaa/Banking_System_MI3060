4a0d537f167f9e4e14cef193cd00dfc2119956d6
Bonkaa <kinnguynhu@gmail.com>
2025-06-06 15:34:30 +0700

    finishing main.cpp and fixing all the bugs occuried

---
+++ build/CMakeFiles/BankingSystem.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "BankingSystem.exe"
+  "BankingSystem.exe.manifest"
+  "BankingSystem.pdb"
+  "CMakeFiles/BankingSystem.dir/Account/Account.cpp.obj"
+  "CMakeFiles/BankingSystem.dir/Account/Account.cpp.obj.d"
+  "CMakeFiles/BankingSystem.dir/Bank/Bank.cpp.obj"
+  "CMakeFiles/BankingSystem.dir/Bank/Bank.cpp.obj.d"
+  "CMakeFiles/BankingSystem.dir/Menu/Menu.cpp.obj"
+  "CMakeFiles/BankingSystem.dir/Menu/Menu.cpp.obj.d"
+  "CMakeFiles/BankingSystem.dir/Transaction/Transaction.cpp.obj"
+  "CMakeFiles/BankingSystem.dir/Transaction/Transaction.cpp.obj.d"
+  "CMakeFiles/BankingSystem.dir/main.cpp.obj"
+  "CMakeFiles/BankingSystem.dir/main.cpp.obj.d"
+  "libBankingSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BankingSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
